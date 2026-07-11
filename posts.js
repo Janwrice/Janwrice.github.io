0aa9edbc267f91ac201f6d0e2a455c4fa6350bd8
Janwrice <77104205+Janwrice@users.noreply.github.com>
2026-07-11 12:02:44 +0800

    Add files via upload

--- posts.js
+++ posts.js
@@ -1,3 +1,9 @@
 window.__POSTS__ = [
-  { "title": "SQL 全面参考指南", "date": "2026-07-10", "url": "posts/sql-reference.html" }
+  { "title": "高等数学知识点梳理", "date": "2026-07-11", "url": "posts/am-review.html" },
+  { "title": "SQL 全面参考指南", "date": "2026-07-10", "url": "posts/sql-reference.html" },
+  { "title": "Kate 编辑器简单配置", "date": "2024-09", "url": "https://linux.do/t/topic/199225" },
+  { "title": "在联想小新上使用 debian Linux", "date": "2024-09", "url": "https://linux.do/t/topic/199149" },
+  { "title": ".apks 初探", "date": "2022-12-02", "url": "https://www.bilibili.com/opus/735076497466851366" },
+  { "title": "使用 Termux 远程收发短信", "date": "2022-11-07", "url": "https://www.bilibili.com/opus/725661052993274032" },
+  { "title": "Minecraft 基岩版正版验证机制分析与绕过技术", "date": "2021-08-26", "url": "posts/mc-bypass-license.html" }
 ];
